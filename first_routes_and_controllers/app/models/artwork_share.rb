962628fb3792ad5c73857ef29491a362a2b72b14
Steven Inouye <steveninouye@msn.com>
2018-11-12 17:00:02 -0800

    Add artwork share model

--- first_routes_and_controllers/app/models/artwork_share.rb
+++ first_routes_and_controllers/app/models/artwork_share.rb
@@ -13,9 +13,9 @@ class ArtworkShare < ApplicationRecord
   belongs_to :artwork,
     foreign_key: :artwork_id,
     class_name: :Artwork
-    
+
   belongs_to :viewer,
     foreign_key: :viewer_id,
     class_name: :User
-  
+
 end
