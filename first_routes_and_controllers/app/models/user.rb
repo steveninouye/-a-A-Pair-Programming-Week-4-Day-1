137f9d48c1e54d28ee6f43565d52aff0c0c79275
Steven Inouye <steveninouye@msn.com>
2018-11-12 16:59:41 -0800

    Add Create Artwork Shares migration

--- first_routes_and_controllers/app/models/user.rb
+++ first_routes_and_controllers/app/models/user.rb
@@ -10,15 +10,15 @@
 
 class User < ApplicationRecord
   validates :username, presence: true, uniqueness: true
-  
+
   has_many :artworks,
     foreign_key: :artist_id,
     class_name: :Artwork,
     primary_key: :id
-    
+
   has_many :artwork_shares,
     foreign_key: :viewer_id,
     class_name: :ArtworkShare
-    
+
 
 end
